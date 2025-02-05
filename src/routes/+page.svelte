90830d648145a876a135c29d4788a6490e0f8682
Anthony Headley <agheadley@gmail.com>
2025-02-05 12:21:45 +0000

    base build

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,21 @@
 <script lang="ts">
 	
+let testRead = async()=>{
+	let response = await fetch('/edge/read', {
+		method: 'POST',
+		body: JSON.stringify({table:"test_person_table",select:"*"}),
+		headers: {'content-type': 'application/json'}
+	});
+	let data = await response.json();
+	console.log('testRead()',data);
+};
+$effect(()=>{
+	console.log('+page.svelte mounted');
+	testRead();
+
+	
+});
+
 </script>
 
 <svelte:head>
